5aff2996875eee9fee666b1d64b4daf5ac39e70b
Ilari Matikainen <ilari.matikainen@rokki.net>
2023-01-18 15:34:06 +0200

    lint fix

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -6,106 +6,106 @@ describe('Blog ', function() {
       username: 'aappa123',
       password: 'monkeyman'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user) 
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-    it('Login form is shown', function() {
-      cy.contains('Login:')
-      cy.contains('Username:')
-      cy.contains('Password:')
-      cy.contains('Login')
+  it('Login form is shown', function() {
+    cy.contains('Login:')
+    cy.contains('Username:')
+    cy.contains('Password:')
+    cy.contains('Login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('aappa123')
+      cy.get('#password').type('monkeyman')
+      cy.get('#login-button').click()
+
+      cy.contains('Logged in as aappa123')
     })
 
-    describe('Login',function() {
-      it('succeeds with correct credentials', function() {
-        cy.get('#username').type('aappa123')
-        cy.get('#password').type('monkeyman')
-        cy.get('#login-button').click()
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('aappa123')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
 
-        cy.contains('Logged in as aappa123')
-      })
-  
-      it('fails with wrong credentials', function() {
-        cy.get('#username').type('aappa123')
-        cy.get('#password').type('wrong')
-        cy.get('#login-button').click()
+      cy.contains('Invalid username or password!')
+    })
+  })
 
-        cy.contains('Invalid username or password!')
-      })
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.get('#username').type('aappa123')
+      cy.get('#password').type('monkeyman')
+      cy.get('#login-button').click()
     })
 
-    describe('When logged in', function() {
-      beforeEach(function() {
-        cy.get('#username').type('aappa123')
-        cy.get('#password').type('monkeyman')
-        cy.get('#login-button').click()
-      })
-  
-      it('A blog can be created', function() {
-        cy.contains('Create new blog').click()
-        cy.get('#title').type('Otsikko')
-        cy.get('#author').type('Kirjoittaja')
-        cy.get('#url').type('Osoite')
-        cy.contains('Add blog').click()
+    it('A blog can be created', function() {
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Otsikko')
+      cy.get('#author').type('Kirjoittaja')
+      cy.get('#url').type('Osoite')
+      cy.contains('Add blog').click()
 
-        cy.contains('Otsikko, written by Kirjoittaja')
-      })
+      cy.contains('Otsikko, written by Kirjoittaja')
+    })
 
-      it('A blog can be liked', function() {
-        cy.contains('Create new blog').click()
-        cy.get('#title').type('Otsikko')
-        cy.get('#author').type('Kirjoittaja')
-        cy.get('#url').type('Osoite')
-        cy.contains('Add blog').click()
-        cy.contains('Show').click()
-        cy.contains('Like').click()
-        cy.visit('http://localhost:3000')
-        cy.contains('Show').click()
+    it('A blog can be liked', function() {
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Otsikko')
+      cy.get('#author').type('Kirjoittaja')
+      cy.get('#url').type('Osoite')
+      cy.contains('Add blog').click()
+      cy.contains('Show').click()
+      cy.contains('Like').click()
+      cy.visit('http://localhost:3000')
+      cy.contains('Show').click()
 
-        cy.contains('Likes:1')
-      })
+      cy.contains('Likes:1')
+    })
 
-      it('A blog can be removed', function() {
-        cy.contains('Create new blog').click()
-        cy.get('#title').type('Otsikko')
-        cy.get('#author').type('Kirjoittaja')
-        cy.get('#url').type('Osoite')
-        cy.contains('Add blog').click()
-        cy.visit('http://localhost:3000')
-        cy.contains('Show').click()
-        cy.contains('Remove blog').click()
-        cy.on('window:confirm', () => true)
-        cy.visit('http://localhost:3000')
-        
-        cy.contains('Otsikko').should('not.exist')
-      })
+    it('A blog can be removed', function() {
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Otsikko')
+      cy.get('#author').type('Kirjoittaja')
+      cy.get('#url').type('Osoite')
+      cy.contains('Add blog').click()
+      cy.visit('http://localhost:3000')
+      cy.contains('Show').click()
+      cy.contains('Remove blog').click()
+      cy.on('window:confirm', () => true)
+      cy.visit('http://localhost:3000')
 
-      it('Blogs are sorted according to likes', function() {
-        cy.contains('Create new blog').click()
-        cy.get('#title').type('Otsikko')
-        cy.get('#author').type('Kirjoittaja')
-        cy.get('#url').type('Osoite')
-        cy.contains('Add blog').click()
+      cy.contains('Otsikko').should('not.exist')
+    })
 
-        cy.visit('http://localhost:3000')
-        cy.contains('Create new blog').click()
-        cy.get('#title').type('Parempi')
-        cy.get('#author').type('Tyyppi')
-        cy.get('#url').type('like.com')
-        cy.contains('Add blog').click()
-        cy.contains('Show').click()
-        cy.contains('Show').click()
-        cy.contains('Close').click()
-        cy.contains('Like').click()
+    it('Blogs are sorted according to likes', function() {
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Otsikko')
+      cy.get('#author').type('Kirjoittaja')
+      cy.get('#url').type('Osoite')
+      cy.contains('Add blog').click()
 
-        cy.visit('http://localhost:3000')
-        cy.contains('Show').click()
-        
-        cy.contains('Url:like.com')
-      })
+      cy.visit('http://localhost:3000')
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Parempi')
+      cy.get('#author').type('Tyyppi')
+      cy.get('#url').type('like.com')
+      cy.contains('Add blog').click()
+      cy.contains('Show').click()
+      cy.contains('Show').click()
+      cy.contains('Close').click()
+      cy.contains('Like').click()
 
+      cy.visit('http://localhost:3000')
+      cy.contains('Show').click()
+
+      cy.contains('Url:like.com')
     })
 
   })
+
+})
 
--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -2,20 +2,19 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
-import userEvent from '@testing-library/user-event'
 
 test('Only title and author are rendered by default', async () => {
-    const blog = {
-        title: 'Titteli',
-        author: 'Autoori',
-        url: 'urli',
-        likes: 10,
-        user: 'abc123'
-    }
+  const blog = {
+    title: 'Titteli',
+    author: 'Autoori',
+    url: 'urli',
+    likes: 10,
+    user: 'abc123'
+  }
 
-    render(<Blog blog={blog} />)
+  render(<Blog blog={blog} />)
 
-    const titleauthor = screen.getByText('Titteli, written by Autoori')
-    expect(titleauthor).toBeDefined()
+  const titleauthor = screen.getByText('Titteli, written by Autoori')
+  expect(titleauthor).toBeDefined()
 })
 
